43c0927de07aa438d47f2409ae3f1d7d2e9267f8
MatheusHenrique564 <matheus.henrique.senai564@gmail.com>
2024-04-22 11:32:41 -0300

    first commit

---
+++ db/script.sql
@@ -0,0 +1,13 @@
+
+
+CREATE TABLE usuarios (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    surname VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    birthdate DATE NOT NULL,
+    age INTEGER NOT NULL,
+    sign VARCHAR(100) NOT NULL
+);
+
+INSERT INTO usuarios (nome, email) VALUES ('Juan', 'Juan@gamil.com');
